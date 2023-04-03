b41e03062fd5d8e11a620ab94a400934fc2180e8
Jean Gravian <defreitas.flavio7@gmail.com>
2023-04-03 16:01:27 +0200

    Blur + image

--- components/Feed/Feed.jsx
+++ components/Feed/Feed.jsx
@@ -1,7 +1,7 @@
-import {BsStars} from 'react-icons/bs'
-import Input from '../Input/Input'
-import Post from '../Post/Post'
-import React from 'react'
+import { BsStars } from "react-icons/bs";
+import Input from "../Input/Input";
+import Post from "../Post/Post";
+import React from "react";
 
 const Feed = () => {
   const posts = [
@@ -9,7 +9,8 @@ const Feed = () => {
       id: "1",
       name: "Graig Kolodziejczyk",
       username: "GraigCode",
-      userImg: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F220453%2Fpexels-photo-220453.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-pixabay-220453.jpg%26fm%3Djpg&tbnid=QsatBu9zpFWl5M&vet=12ahUKEwjAzYPw5er9AhUTmicCHSSHAqgQMygDegUIARDoAQ..i&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fprofile%2F&docid=y7i4Swccn0rqpM&w=3476&h=5214&q=profile%20image&ved=2ahUKEwjAzYPw5er9AhUTmicCHSSHAqgQMygDegUIARDoAQ",
+      userImg:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
       img: "https://pbs.twimg.com/media/FrnHITZWcAACaKG?format=jpg&name=small",
       text: "Nice job",
       timestamp: "2 hours ago",
@@ -18,8 +19,9 @@ const Feed = () => {
       id: "2",
       name: "Graig Kolodziejczyk",
       username: "GraigCode",
-      userImg: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fphotos%2F220453%2Fpexels-photo-220453.jpeg%3Fcs%3Dsrgb%26dl%3Dpexels-pixabay-220453.jpg%26fm%3Djpg&tbnid=QsatBu9zpFWl5M&vet=12ahUKEwjAzYPw5er9AhUTmicCHSSHAqgQMygDegUIARDoAQ..i&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fsearch%2Fprofile%2F&docid=y7i4Swccn0rqpM&w=3476&h=5214&q=profile%20image&ved=2ahUKEwjAzYPw5er9AhUTmicCHSSHAqgQMygDegUIARDoAQ",
-      img: "https://pbs.twimg.com/media/FrnHITZWcAACaKG?format=jpg&name=small",
+      userImg:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+      img: "https://pbs.twimg.com/media/FsyDuv5XwAEMOep?format=jpg&name=large",
       text: "Nice job!!",
       timestamp: "2 days ago",
     },
@@ -27,26 +29,29 @@ const Feed = () => {
       id: "3",
       name: "Graig Kolodziejczyk",
       username: "GraigCode",
-      userImg: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.licdn.com%2Fdms%2Fimage%2FC4E03AQFIz1CRoh5YSg%2Fprofile-displayphoto-shrink_800_800%2F0%2F1663779779208%3Fe%3D2147483647%26v%3Dbeta%26t%3DTxSy6VYDjXqByTaKxD6J6hBQYjk4YZc55Iq-iPpgEP8&tbnid=9KSnBhoAYRgpuM&vet=12ahUKEwifgL2bqOv9AhXEpicCHQTsB68QMygAegQIARAn..i&imgrefurl=https%3A%2F%2Ffr.linkedin.com%2Fin%2Fgraig-kolodziejczyk-1482241b8&docid=CzXKJgPKRmiFsM&w=800&h=800&q=graig%20kolodziejczyk&ved=2ahUKEwifgL2bqOv9AhXEpicCHQTsB68QMygAegQIARAn",
-      img:"https://pbs.twimg.com/media/FrnHITZWcAACaKG?format=jpg&name=small",
+      userImg:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg",
+      img: "https://pbs.twimg.com/media/FsteoeCXgAIYGVE?format=jpg&name=small",
       text: "Nice job!!!!",
       timestamp: "2 minutes ago",
     },
   ];
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-700 xl:min-w-[576px] sm:ml-[73px] flew-grow max-w-xl">
-        <div className="flex py-2 px-3 sticky top-0 z-50 justify-between border-b border-gray-700">
-            <h2 className="text-lg sm:text-xl font-bold cursor-pointer sticky">Home</h2>
-            <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
-                <BsStars className="h-5"/>
-            </div>
+      <div className="flex align-middle py-4 px-6 sticky top-0 z-50 justify-around border-b border-gray-700 backdrop-blur-md">
+        <h2 className="text-lg sm:text-xl font-bold cursor-pointer sticky">
+          Home
+        </h2>
+        <div className="hoverEffect flex items-center justify-center px-0 ml-auto w-9 h-9">
+          <BsStars className="h-5" />
         </div>
-        <Input />
-        {posts.map((post) => {
-          return <Post key={post.id} post={post} />
-          })}
+      </div>
+      <Input />
+      {posts.map((post) => {
+        return <Post key={post.id} post={post} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
--- components/Post/Post.jsx
+++ components/Post/Post.jsx
@@ -1,35 +1,41 @@
-import { BsChatDots, BsShare, BsTrash } from 'react-icons/bs'
+import { BsChatDots, BsShare, BsTrash } from "react-icons/bs";
 
-import {AiOutlineHeart} from 'react-icons/ai'
-import { FiMoreHorizontal } from 'react-icons/fi'
-import { IoMdStats } from 'react-icons/io'
-import React from 'react'
+import { AiOutlineHeart } from "react-icons/ai";
+import { FiMoreHorizontal } from "react-icons/fi";
+import { IoMdStats } from "react-icons/io";
+import React from "react";
 
-const Post = ({post}) => {
+const Post = ({ post }) => {
   return (
-    <div className="flex p-3 cursor-pointer border-b border-gray-200">
-    <img src={post.userImg} className="h-11 w-11 rounded-full mr-4"/>
-    <div className="flex-1">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center space-x-1 whitespace-nowrap">
-          <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.name}</h4>
-          <span className="text-sm sm:text-[15px]">@{post.username} - </span>
-          <span className="text-sm sm:text-[15px] hover:underline">{post.timestamp}</span>
+    <div className="flex p-3 cursor-pointer border-b border-gray-700">
+      <img src={post.userImg} className="h-11 w-11 rounded-full mr-4" />
+      <div className="flex-1">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center space-x-1 whitespace-nowrap">
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+              {post.name}
+            </h4>
+            <span className="text-sm sm:text-[15px]">@{post.username} - </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+              {post.timestamp}
+            </span>
+          </div>
+          <FiMoreHorizontal className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
         </div>
-        <FiMoreHorizontal className="h-10 hoverEffect w-10 hover:bg-sky-100 hover:text-sky-500 p-2 " />
-      </div>
-      <p className="text-gray-800 text-[15px sm:text-[16px] mb-2 text-white">{post.text}</p>
-      <img src={post.img} className="rounded-2xl mr-2"/>
-      <div className="flex justify-between text-gray-500 p-2">
-        <BsChatDots className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"/>
-        <BsShare className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-red-100"/>
-        <AiOutlineHeart className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-red-100" />
-        <BsTrash className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
-        <IoMdStats className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+        <p className="text-gray-800 text-[15px sm:text-[16px] mb-2 text-white">
+          {post.text}
+        </p>
+        <img src={post.img} className="rounded-2xl mr-2" />
+        <div className="flex justify-between text-gray-500 p-2">
+          <BsChatDots className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <BsShare className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-red-100" />
+          <AiOutlineHeart className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-red-100" />
+          <BsTrash className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <IoMdStats className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+        </div>
       </div>
     </div>
-  </div>
   );
-}
+};
 
-export default Post
+export default Post;
